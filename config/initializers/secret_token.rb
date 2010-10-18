fe9130c5ce099955d43075b41ace34e51fa85ac3
Ross Hale <dev@lottay.com>
2010-10-18 13:53:01 -0700

    blank project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suenophoto::Application.config.secret_token = 'c2c3a05ae10e2e1d631ec9f6844fa37434dccf9caf80f326df5b22561601909d4cabbbe9e0a864d7b1fa1af92c09d09b4a15024415cedd30021d1eff03b80277'
